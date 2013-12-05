fdfd1c523ccd56f0efc20f6c7336c3a12c8159c9
gustavodreier <gustavodreier@gmail.com>
2013-12-05 21:53:04 -0200

    Update Pilha.java

--- Pilha.java
+++ Pilha.java
@@ -1,53 +1,53 @@
 public class Pilha {
 
-	private double[] dados;
-	private int topo;
-	
-	public Pilha (int tam)
-	{
-		dados = new double[tam];
-		topo = 0;
-	}
-	
-	//ve o ultimo valor inserido na pilha (peek = espiar)
-	//não altera para operador
-	public double peek () throws PilhaVazia
-	{
-		if (isEmpty())
-			throw new PilhaVazia();
-		
-		return dados[topo - 1]; 
-	}
+        private double[] dados;
+        private int topo;
+        
+        public Pilha (int tam)
+        {
+                dados = new double[tam];
+                topo = 0;
+        }
+        
+        //ve o ultimo valor inserido na pilha (peek = espiar)
+        //nÃ£o altera para operador
+        public double peek () throws PilhaVazia
+        {
+                if (isEmpty())
+                        throw new PilhaVazia();
+                
+                return dados[topo - 1]; 
+        }
 
-	//insere um elemento na pilha na proxima posição
-	//deve ser passado um valor para o metodo
-	//para operador deve passar uma String
-	public void push (double valor) throws PilhaCheia
-	{
-		if (isFull())
-			throw new PilhaCheia();
-		
-		dados[topo] = valor;
-		topo++;
-	}
-	
-	//remove o ultimo valor da pilha
-	
-	public double pop () throws PilhaVazia
-	{
-		if (isEmpty())
-			throw new PilhaVazia();
-		
-		topo--;
-		return dados[topo];
-	}
-	
-	//pilha esta cheia
-	private boolean isFull() {
-		return topo == dados.length;
-	}
+        //insere um elemento na pilha na proxima posiÃ§Ã£o
+        //deve ser passado um valor para o metodo
+        //para operador deve passar uma String
+        public void push (double valor) throws PilhaCheia
+        {
+                if (isFull())
+                        throw new PilhaCheia();
+                
+                dados[topo] = valor;
+                topo++;
+        }
+        
+        //remove o ultimo valor da pilha
+        
+        public double pop () throws PilhaVazia
+        {
+                if (isEmpty())
+                        throw new PilhaVazia();
+                
+                topo--;
+                return dados[topo];
+        }
+        
+        //pilha esta cheia
+        private boolean isFull() {
+                return topo == dados.length;
+        }
 
-	private boolean isEmpty() {
-		return topo == 0;
-	}
+        private boolean isEmpty() {
+                return topo == 0;
+        }
 }
