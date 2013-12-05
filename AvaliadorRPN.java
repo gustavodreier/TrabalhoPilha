f184ef8dcde9dbb91fe824c036ccc26d6e44d851
gustavodreier <gustavodreier@gmail.com>
2013-12-05 21:51:57 -0200

    Update AvaliadorRPN.java

--- AvaliadorRPN.java
+++ AvaliadorRPN.java
@@ -1,35 +1,34 @@
-
 import java.util.Scanner;
 
 public class AvaliadorRPN {
-	public static double avalia (String expressao) throws PilhaCheia, PilhaVazia, OperadorInvalido
-	{	
-		Pilha pilha = new Pilha(50);
-		
-		Scanner sc = new Scanner(expressao);
-		
-		while (sc.hasNext()) {
-			if (sc.hasNextInt()) {
-				pilha.push(sc.nextInt());
-			} else {
-				String op = sc.next();
-				double rhs = pilha.pop();
-				double lhs = pilha.pop();
-				pilha.push( executaOperador(op.charAt(0), lhs, rhs) );
-			}
-		}
-		
-		return pilha.pop();
-	}
+        public static double avalia (String expressao) throws PilhaCheia, PilhaVazia, OperadorInvalido
+        {        
+                Pilha pilha = new Pilha(50);
+                
+                Scanner sc = new Scanner(expressao);
+                
+                while (sc.hasNext()) {
+                        if (sc.hasNextInt()) {
+                                pilha.push(sc.nextInt());
+                        } else {
+                                String op = sc.next();
+                                double rhs = pilha.pop();
+                                double lhs = pilha.pop();
+                                pilha.push( executaOperador(op.charAt(0), lhs, rhs) );
+                        }
+                }
+                
+                return pilha.pop();
+        }
 
-	private static double executaOperador(char op, double lhs, double rhs) throws OperadorInvalido
-	{
-		switch (op) {
-			case '+': return lhs + rhs;
-			case '-': return lhs - rhs;
-			case '*': return lhs * rhs;
-			case '/': return lhs / rhs;
-			default: throw new OperadorInvalido(op);
-		}
-	}
+        private static double executaOperador(char op, double lhs, double rhs) throws OperadorInvalido
+        {
+                switch (op) {
+                        case '+': return lhs + rhs;
+                        case '-': return lhs - rhs;
+                        case '*': return lhs * rhs;
+                        case '/': return lhs / rhs;
+                        default: throw new OperadorInvalido(op);
+                }
+        }
 }
